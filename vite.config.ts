487fdb847ecd0c2cf51aeccb0ad80b895b43dd2a
Abdillah-Ali <abdillahx132x@gmail.com>
2026-02-19 18:44:24 +0300

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Sarnoff-Property-Tax-Estimator/",
   server: {
     host: "::",
     port: 8080,
